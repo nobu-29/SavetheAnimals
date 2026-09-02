033c3586523fdd9c432e54cf14f1047208304699
nobu-29 <20234144@ms.kcska.ac.jp>
2026-09-02 11:39:19 +0900

    プロジェクト開始

---
+++ Source/SavetheAnimals/ProgramFailes.cpp
@@ -0,0 +1,12 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "ProgramFailes.h"
+
+ProgramFailes::ProgramFailes()
+{
+}
+
+ProgramFailes::~ProgramFailes()
+{
+}
