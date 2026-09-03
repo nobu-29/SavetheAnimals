11dfd65da9dfd9000ab6d5287c17ea2d4b670636
nobu-29 <20234144@ms.kcska.ac.jp>
2026-09-03 13:24:50 +0900

    プログラムを追加できる用のブループリントのクラス設定完了

---
+++ Source/SavetheAnimals/Private/Programs.cpp
@@ -0,0 +1,27 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Programs.h"
+
+// Sets default values
+APrograms::APrograms()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void APrograms::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void APrograms::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
